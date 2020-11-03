8ad6f189d90c135a884006b8a73c31f04b5758bb
Meaghan Neylon <mneylon@richmond.edu>
2020-11-03 11:54:37 -0500

    Established a connection.

---
+++ server.js
@@ -0,0 +1,17 @@
+var express = require("express");
+
+
+var app = express();
+var PORT = process.env.PORT || 3500; 
+
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static(__dirname)); 
+
+//require('./routes/routes')(app); 
+//require('./routes/htmlRoutes')(app); 
+
+app.listen(PORT, function() {
+    console.log("App is listening on PORT: " + PORT);
+});
